1ec24f5bc939c3e0f5a1ad0f3628928b68ca809d
zaryab <zaryabkhan248@gmail.com>
2025-11-24 17:40:49 +0500

    cc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // allow 'any'
+      "@typescript-eslint/no-unused-vars": "off", // allow unused variables
+      "no-console": "off", // allow console.log
+      "no-undef": "off", // allow undefined variables (optional)
+    },
+  },
 ];
 
 export default eslintConfig;
