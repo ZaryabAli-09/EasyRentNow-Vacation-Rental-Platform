1e1d9d37de5363efd59bebd1c75f3d6dbe4e30c5
zaryab <zaryabkhan248@gmail.com>
2025-11-24 09:54:38 +0500

    category nav enhanced

--- app/custom components/MapFilterItems.tsx
+++ app/custom components/MapFilterItems.tsx
@@ -3,15 +3,22 @@
 import { categoryItems } from "@/lib/categoryItems";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export function MapFilterItems() {
+  const pathName = usePathname();
+  const categoryPath = pathName.split("/")[2];
   return (
-    <div className="flex gap-x-10 mt-5 w-full overflow-x-scroll no-scrollbar">
+    <div className="flex gap-x-10 mt-5 w-full overflow-x-scroll no-scrollbar justify-center">
       {categoryItems.map((item) => (
         <Link
           key={item.id}
           href={`/category/${item.name}`}
-          className="flex flex-col gap-y-3 items-center opacity-85 hover:opacity-100"
+          className={`flex flex-col gap-y-3 items-center opacity-85 hover:opacity-100 ${
+            categoryPath === item.name
+              ? "border-b-4 border-pink-700 opacity-100"
+              : ""
+          }`}
         >
           <Image src={item.imageUrl} width={24} height={24} alt={item.title} />
           <p className="text-xs font-medium">{item.title}</p>
